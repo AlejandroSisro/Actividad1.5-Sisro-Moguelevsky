adcffc34e7b58cdd0a28a2db9cf2ca74518e3755
AlejandroSisro <50434059@est.ort.edu.ar>
2026-03-18 08:24:05 -0300

    Add files via upload

---
+++ Curso.cs
@@ -0,0 +1,61 @@
+public class Curso
+{
+    private List<Alumno> alumnos;
+
+    public Curso()
+    {
+        alumnos = new List<Alumno>();
+    }
+    public bool AgregarAlumno(Alumno alumno)
+    {
+        int i = 0;
+        bool alumnoEncontrado = false;
+        while (i < alumnos.Count && alumnoEncontrado == false)
+        {
+            if (alumno.obtenerDNI() == alumnos[i].obtenerDNI())
+            {
+                alumnoEncontrado = true;
+            }
+            i++
+        }
+        if (alumnoEncontrado == false)
+        {
+            alumnos.Add(alumno);
+        }
+        return alumnoEncontrado;
+    }
+    public bool BuscarAlumno(Alumno alumno)
+    {
+        int i = 0;
+        bool alumnoEncontrado = false;
+        while (i < alumnos.Count && alumnoEncontrado == false)
+        {
+            if (alumno.obtenerDNI() == alumnos[i].obtenerDNI())
+            {
+                alumnoEncontrado = true;
+            }
+            i++
+        }
+        return alumnoEncontrado;
+    }
+    public void MostrarAlumnos()
+    {
+        for(int i = 0; i < alumnos.count; i++)
+        {
+            Console.WriteLine(alumnos[i].getNombre());
+        }
+    }
+    public bool MostrarAlumnosCon15Faltas()
+    {
+        int i = 0;
+
+        while (i < alumnos.Count)
+        {
+            if (alumnos[i].obtenerFaltas() = 15)
+            {
+                Console.WriteLine(alumnos[i].getNombre());
+            }
+            i++
+        }
+    }
+}
